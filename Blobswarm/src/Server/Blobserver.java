869a2eed106ec12a24003ffe7e766d3e00ddeee8
TomasRingefelt <tomas.ringefelt@gmail.com>
2014-05-13 14:45:19 +0200

    Refactoring. Client handler extracted from server.

--- Blobswarm/src/Server/Blobserver.java
+++ Blobswarm/src/Server/Blobserver.java
@@ -27,7 +27,6 @@ import com.esotericsoftware.minlog.Log;
 
 public class Blobserver {
 	Server server;
-	int blobIDs = 0;
 	int FPS = 30;
 	World world;
 	
@@ -49,54 +48,7 @@ public class Blobserver {
 		// registered by the same method for both the client and server.
 		Network.register(server);
 
-		server.addListener(new Listener() {
-			public void received (Connection c, Object object) {
-				// We know all connections for this server are actually ChatConnections.
-				ChatConnection connection = (ChatConnection)c;
-
-				if (object instanceof ServerInput) {
-					if (connection.name == null) return;
-					ServerInput input = ((ServerInput)object);		
-					if (input.up)
-							world.attemptMove(connection.blob, 1);
-					if (input.down)             // Do this if the input is the down button.
-							world.attemptMove(connection.blob, 2);
-					if (input.left)            // Do this if the input is the left button.
-							world.attemptMove(connection.blob, 3);
-					if (input.right)         // Do this if the input is the right button.
-							world.attemptMove(connection.blob, 4);
-											
-					return;
-					}
-				
-				
-				if (object instanceof RegisterName) {
-					// Ignore the object if a client has already registered a name. This is
-					// impossible with our client, but a hacker could send messages at any time.
-					if (connection.name != null) return;
-					// Ignore the object if the name is invalid.
-					String name = ((RegisterName)object).name;
-					if (name == null) return;
-					name = name.trim();
-					if (name.length() == 0) return;
-					// Store the name on the connection.
-					connection.name = name;
-					connection.blob = new Blob(connection.name);
-					connection.blob.setID(blobIDs);
-					world.spawnLocation(connection.blob);
-					blobIDs += 1;
-					// Send a "connected" message to everyone except the new client.
-					ChatMessage chatMessage = new ChatMessage();
-					chatMessage.text = name + " connected.";
-					server.sendToAllExceptTCP(connection.getID(), chatMessage);
-					// Send everyone a new list of connection names.
-					updateNames();
-					return;
-				}
-
-			}
-
-		});
+		server.addListener(new ClientHandler(world, server));
 		server.bind(Network.port);
 		server.start();
 
@@ -145,24 +97,9 @@ public class Blobserver {
 		
 	}
 
-	void updateNames () {
-		//x marked comments to update something every players pos.
-		// Collect the names for each connection.
-		Connection[] connections = server.getConnections();
-		int totalConnections = connections.length; //x
-		ArrayList<String> names = new ArrayList<String>(totalConnections);//x
-		world.setPlayers(new Blob[totalConnections]);//x
-		for (int i = connections.length - 1; i >= 0; i--) {
-			ChatConnection connection = (ChatConnection)connections[i];
-			names.add(connection.name);
-			world.getPlayers()[i] = connection.blob;//x
-		}
-		// Send the names to everyone.
-		UpdateNames updateNames = new UpdateNames();
-		updateNames.names = (String[])names.toArray(new String[names.size()]);
-		server.sendToAllTCP(updateNames);
-	}
+	
 
+	
 	// This holds per connection state.
 	public static class ChatConnection extends Connection {
 		public String name;
---
+++ Blobswarm/src/Server/ClientHandler.java
@@ -0,0 +1,89 @@
+package Server;
+
+import java.util.ArrayList;
+
+import Common.Blob;
+import Common.Network.ChatMessage;
+import Common.Network.RegisterName;
+import Common.Network.ServerInput;
+import Common.Network.UpdateNames;
+import Server.Blobserver.ChatConnection;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+
+public class ClientHandler extends Listener {
+	private Server server;
+	private World world;
+	private static int blobIDs = 0;
+	
+	public ClientHandler(World world, Server server){
+		this.world = world;
+		this.server = server;
+	}
+	
+	public void received (Connection c, Object object) {
+		// We know all connections for this server are actually ChatConnections.
+		ChatConnection connection = (ChatConnection)c;
+
+		if (object instanceof ServerInput) {
+			if (connection.name == null) return;
+			ServerInput input = ((ServerInput)object);		
+			if (input.up)
+					world.attemptMove(connection.blob, 1);
+			if (input.down)             // Do this if the input is the down button.
+					world.attemptMove(connection.blob, 2);
+			if (input.left)            // Do this if the input is the left button.
+					world.attemptMove(connection.blob, 3);
+			if (input.right)         // Do this if the input is the right button.
+					world.attemptMove(connection.blob, 4);
+									
+			return;
+			}
+		
+		
+		if (object instanceof RegisterName) {
+			// Ignore the object if a client has already registered a name. This is
+			// impossible with our client, but a hacker could send messages at any time.
+			if (connection.name != null) return;
+			// Ignore the object if the name is invalid.
+			String name = ((RegisterName)object).name;
+			if (name == null) return;
+			name = name.trim();
+			if (name.length() == 0) return;
+			// Store the name on the connection.
+			connection.name = name;
+			connection.blob = new Blob(connection.name);
+			connection.blob.setID(blobIDs);
+			world.spawnLocation(connection.blob);
+			blobIDs += 1;
+			// Send a "connected" message to everyone except the new client.
+			ChatMessage chatMessage = new ChatMessage();
+			chatMessage.text = name + " connected.";
+			server.sendToAllExceptTCP(connection.getID(), chatMessage);
+			// Send everyone a new list of connection names.
+			updateNames();
+			return;
+		}
+
+	}
+
+	private void updateNames () {
+		//x marked comments to update something every players pos.
+		// Collect the names for each connection.
+		Connection[] connections = server.getConnections();
+		int totalConnections = connections.length; //x
+		ArrayList<String> names = new ArrayList<String>(totalConnections);//x
+		world.setPlayers(new Blob[totalConnections]);//x
+		for (int i = connections.length - 1; i >= 0; i--) {
+			ChatConnection connection = (ChatConnection)connections[i];
+			names.add(connection.name);
+			world.getPlayers()[i] = connection.blob;//x
+		}
+		// Send the names to everyone.
+		UpdateNames updateNames = new UpdateNames();
+		updateNames.names = (String[])names.toArray(new String[names.size()]);
+		server.sendToAllTCP(updateNames);
+	}
+}
